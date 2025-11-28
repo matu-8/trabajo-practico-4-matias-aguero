5390bf11a8932b166b1e8b08339f542c00696a2f
Matias Agüero <agueromatias214@gmail.com>
2025-11-28 12:27:52 -0300

    Realizada la actividad 2 que implica funciones tipadas en typescript

---
+++ src/actividad-2/funciones-tipadas.ts
@@ -0,0 +1,6 @@
+
+function updateStock(currentStock: number, quantityChange: number):number{
+    return currentStock + quantityChange;
+}
+const result = updateStock(40, 40)
+console.log('Resultado', result)
